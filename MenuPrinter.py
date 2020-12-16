2aa074e84cb3ab81a3a9ed8a74917b94599dc6e9
zlt999 <66427892+zlt999@users.noreply.github.com>
2020-12-16 14:40:28 +0800

    Update MenuPrinter.py

--- MenuPrinter.py
+++ MenuPrinter.py
@@ -8,16 +8,19 @@ b_show_files = True
 
 if __name__ == "__main__":
     t = time.strftime("%H_%M_%S", time.localtime())
-    f_output = open("output{}.txt".format("_MP_" + t), "w", encoding="UTF-8")
+    f_output = open("output{}.txt".format("_FB_" + t), "w", encoding="UTF-8")
     path_root = os.getcwd()
 
     for root, dirs, files in os.walk(path_root):
         if b_show_files:
             for file in files:
-                modif_date = time.localtime(os.stat(os.path.join(root, file)).st_mtime)
-                f_output.write(time.strftime("%Y-%m-%d %H:%M:%S", modif_date) + "\t")
-                path = os.path.join(root, file)
-                f_output.write(path[len(path_root):] + "\n")
+                if file != __file__:
+                    modif_date = time.localtime(os.stat(os.path.join(root, file)).st_mtime)
+                    f_output.write(time.strftime("%Y-%m-%d %H:%M:%S", modif_date) + "\t")
+                    a, suffix = os.path.splitext(file)
+                    f_output.write(suffix[1:]+"\t")
+                    path = os.path.join(root, file)
+                    f_output.write(path[len(path_root):] + "\n")
         if b_show_folders:
             for dir in dirs:
                 path = os.path.join(root, dir)

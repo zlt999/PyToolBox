ad430b24e72a1eeb59dbd05718bf91891e2fbc39
zlt999 <66427892+zlt999@users.noreply.github.com>
2021-01-02 14:27:22 +0800

    Create MenuPrinter.py
    Added openpyxl, can output to excel now

--- MenuPrinter.py
+++ MenuPrinter.py
@@ -32,3 +32,26 @@ if __name__ == "__main__":
                 f_output.write(path[len(path_root):] + "\n")
 
     f_output.close()
+
+    try:
+        import openpyxl
+        f_input = open("Output_MP_{}.txt".format(t),"r",encoding="UTF-8")
+        wb = openpyxl.Workbook()
+        ws = wb.active
+
+        ws.append(["修改时间","文件格式","文件相对地址","文件名"])
+        for line in ws["A1":"F1"]:
+            for cell in line:
+                cell.font = openpyxl.styles.Font(size="16",bold="True")
+        for line in f_input.readlines():
+            ws.append(line.split("\t"))
+
+        wb.save("Output_MP_{}.xlsx".format(t))
+        wb.close()
+        f_input.close()
+        print("搜索完成，已生成文件："+"\n- Output_MP_{}.xlsx".format(t)+"\n- Output_MP_{}.txt".format(t))
+    except ModuleNotFoundError:
+        print("搜索完成，已生成文件：" "\n- Output_MP_{}.txt".format(t))
+        print("\n若要导出为表格，可下载openpyxl库")
+
+    input("\n<< 程序已结束，按任意键退出 >>")
